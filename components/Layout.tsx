c40de3d02c3d850e8d5a81b5c9b8153ebdd8d428
Tom Hine <tomhine792@gmail.com>
2022-06-10 10:09:40 +0100

    first commit

---
+++ components/Layout.tsx
@@ -0,0 +1,32 @@
+import { MoonIcon as DarkIcon } from "@heroicons/react/solid";
+import { MoonIcon as LightIcon } from "@heroicons/react/outline";
+import { useState } from "react";
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+const Layout = ({ children }: LayoutProps) => {
+  const [darkTheme, setDarkTheme] = useState(true);
+
+  const toggleDarkTheme = () => setDarkTheme(theme => !theme);
+
+  return (
+    <div className={`${darkTheme && "dark"}`}>
+      <div className="min-h-screen bg-background-light font-nunito text-text-light dark:bg-background-dark dark:text-text-dark">
+        <header className="flex h-19.5 w-full items-center justify-between bg-element-light px-4  dark:bg-element-dark  md:px-20">
+          <h1 className="font-extrabold">Where in the world?</h1>
+          <button type="button" className="flex items-center gap-2 p-2" onClick={toggleDarkTheme}>
+            {darkTheme ? <DarkIcon className="h-6 w-6" /> : <LightIcon className="h-6 w-6" />}
+            {darkTheme ? "Light Mode" : "Dark Mode"}
+          </button>
+        </header>
+        <main className="mx-auto w-full max-w-7xl px-4 py-6 md:px-19.5 md:py-12 lg:px-0">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        background: {
+          dark: "hsl(207, 26%, 17%)",
+          light: "hsl(0, 0%, 98%)",
+        },
+        element: {
+          dark: "hsl(209, 23%, 22%)",
+          light: "hsl(0, 0%, 100%)",
+        },
+        text: {
+          dark: "hsl(0, 0%, 100%)",
+          light: "hsl(200, 15%, 8%)",
+        },
+        input: {
+          light: "hsl(0, 0%, 52%)",
+        },
+      },
+      fontFamily: {
+        nunito: ["Nunito Sans", "sans-serif"],
+      },
+      spacing: {
+        19.5: "78px",
+      },
+    },
+  },
+  plugins: [],
+};
